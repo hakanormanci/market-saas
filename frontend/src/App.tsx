3bbbe13a0a4f1e8c26be981fe36e57615f50c5f0
hakanormanci <jhakanormanci@gmail.com>
2026-07-01 13:15:11 +0200

    deneme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -72,7 +72,7 @@ export default function App() {
             }
             await triggerAutomaticSearch(decodedText);
           },
-          (_errorMessage) => {
+          (errorMessage) => {
             // Tarama sırasında sürekli tetiklenen logları buraya yazabilirsiniz (Genelde boş bırakılır)
           },
         );
